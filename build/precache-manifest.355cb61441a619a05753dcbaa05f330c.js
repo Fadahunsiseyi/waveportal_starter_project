6e9f7946cc086b9a2d102bc69e1767db32bb483f
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-04-24 01:56:54 +0100

    added the build file for deployment

---
+++ build/precache-manifest.355cb61441a619a05753dcbaa05f330c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8b9319b20baa31834f690b07375b51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc89428a0a816a5dfaf",
+    "url": "/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "fbc89428a0a816a5dfaf",
+    "url": "/static/js/main.2d8deef3.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
